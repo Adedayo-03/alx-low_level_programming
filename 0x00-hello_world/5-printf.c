341c8918aeb1bc164618adc030e7a37fcb170947
Adedayo-03 <princeadedayo03@gmail.com>
2022-10-13 21:06:20 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
-/**
- *
- *  * Main Entry point
- *
- *   *
- *
- *    * Return: 0
- *
- *     */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
