77d728a4ac17c1ff7ec2914ed3636d6872ff9df6
D'Prince <114456625+Adedayo-03@users.noreply.github.com>
2022-10-19 04:43:35 +0100

    print last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,19 +4,14 @@
  * @value: the given value
  * Return: returns 0 if succcessful 1 if otherwise
  */
-int print_last_digit(int value)
+int print_last_digit(int n)
 {
-	int remain = value % 10;
+	int last_digit = n % 10;
 
-	if (remain < 0)
-	{
-		remain = -(remain);
-		return (remain + '0');
-	}
-	else
-	{
-		return (remain + '0');
-	}
-	return (0);
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
-
